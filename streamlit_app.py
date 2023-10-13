e9d4c8e12ddad3c43e6132ddf26c872ae28f933f
Madina Suraya Zharin <madinasuraya@graduate.utm.my>
2023-10-13 09:54:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title("My Parents New Healthy Diner")
+
+st.markdown("# Breakfast Menu")
+
+st.write("""
+Omega 3 & Blueberry Oatmeal
+
+Kale, Spinach & Rocket Smoothie
+
+Hard-Boiled Free-Range Egg
+""")
